453b57b06d0f858c0389df54b2f9c4d4fdc93ff3
Wiley Welch <wileyjwelch1@gmail.com>
2024-08-08 20:11:50 -0600

    Letters to Nums function works and change board is mostly fixed

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ class Game():
     """This is the game object that holds the Tic Tac Toe game."""
     def __init__(self, current_player, board = None):
         self.current_player = current_player
-        self.board = board
+        self.board = [["[]","[]","[]"],["[]","[]","[]"],["[]","[]","[]"]]
         self.avaliable_moves = [["A","1"],["A","2"],["A","3"],["B","1"],["B","2"], ["B","3"], ["C","1"],["C","2"],["C","3"]]
     
     def get_current_player(self):
@@ -17,6 +17,33 @@ class Game():
         """Gets the available moves"""
         return self.avaliable_moves
     
+    def get_board(self):
+        """Gets the Board"""
+        return self.board
+    
+    def change_board(self,row,column,symbol):
+        """When a player makes a legal move this function changes the board to reflect this"""
+        legal_moves = self.get_board()
+        print(legal_moves)
+        current_row = legal_moves[row]
+        print(current_row)
+        changed_board = current_row[:column]+[symbol]+current_row[column+1:]
+        print(changed_board)
+
+    def convert_letters_to_nums(self, row):
+        """Converts ABC to 123"""
+        if row == "A":
+            return 1
+        if row == "B":
+            return 2
+        if row == "C":
+            return 3
+        else:
+            print("Error with Letter Conversion")
+
+
+
+    
     def make_move(self):
         """Makes a move on the board"""
         if self.current_player == "Player":
@@ -25,4 +52,7 @@ class Game():
             cordinate_pair = [row_cordinate,column_cordinate]
             moves = self.get_available_moves()
             if cordinate_pair in moves:
-                print("True")
+                #print("True")
+                row_cordinate = self.convert_letters_to_nums(row_cordinate)
+                self.change_board(row_cordinate,int(column_cordinate),symbol = "[X]")
+
