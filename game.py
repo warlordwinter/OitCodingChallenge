fefe29e59cccec6154dfa783376dd5e3d1615038
Wiley Welch <wileyjwelch1@gmail.com>
2024-08-08 19:05:38 -0600

    tested getters and setters

--- game.py
+++ game.py
@@ -1,3 +1,13 @@
 class Game():
     """This is the game object that holds the Tic Tac Toe game."""
+    def __init__(self, current_player, board = None):
+        self.current_player = current_player
+        self.board = board
     
+    def get_current_player(self):
+        """Gets the current player"""
+        return self.current_player
+    
+    def set_current_player(self, player):
+        """Sets the current player"""
+        self.current_player = player
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import game as game
+
 def main():
+    new_game = game.Game("john", "doe")
+    new_game.set_current_player("jane")
+    print(new_game.get_current_player())
     print("Hello World!")
 
 if __name__ == "__main__":
